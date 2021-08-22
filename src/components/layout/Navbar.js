a015bc4c96c8bfdb6aa46e3046e5fa88f59bbcaf
Bruno Mourinha <53535922+brmourinha@users.noreply.github.com>
2021-08-22 21:51:27 +0100

    testing

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = ({ icon, title }) => {
           <Link to='/'>Home</Link>
         </li>
         <li>
-          <Link to='/about'>About</Link>
+          <Link to='/about'>About P</Link>
         </li>
       </ul>
     </nav>
